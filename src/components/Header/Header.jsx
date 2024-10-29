3efe01bdd8e8ae0bfbd28848afdb701cd32a2e27
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2024-10-29 09:34:54 +0000

    King Image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import './Header.scss'
+import King from 'public/King.png'
 
 const Header = () => {
     return (
         <div className="Header flex flex-items-center flex-justify-between size-a" id="Header">
             <div className='flex pointer-events-none z-1 flex-items-center select-none'>
-                <img className='King pointer-events-none w-16 lg:w-23 inline-block' src='public/King.png' alt="King Image" />
+                <img className='King pointer-events-none w-16 lg:w-23 inline-block' src={King} alt="King Image" />
                 <h3 className="Name font-size-4 lg:font-size-6 inline-block vertical-middle text-white cursor-default">Λʙʜιנєєτ  Ðʜικαℓє</h3>
             </div>
             <a href="public/Documents/AbhijeetPrakashDhikaleResume.pdf" target='_blank' className="Resume z-1 font-size-4">Resume</a>
