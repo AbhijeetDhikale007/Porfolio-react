883f460be4107b11b09859888e39cc6aef6b6199
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-12 13:13:59 +0000

    Improvement: CSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import './Header.scss'
 
 const Header = () => {
     return (
-        <div className="Header flex flex-items-center flex-justify-between size-a" id="Header">
-            <div className='flex pointer-events-none z-1 flex-items-center select-none'>
+        <div className="Header flex items-center justify-between size-a" id="Header">
+            <div className='flex pointer-events-none z-1 items-center select-none'>
                 <img className='King pointer-events-none w-16 lg:w-23 inline-block' src='/King.png' alt="King Image" />
                 <h3 className="Name Roboto-Condensed font-size-4 lg:font-size-6 inline-block vertical-middle text-white cursor-default">Λʙʜιנєєτ  Ðʜικαℓє</h3>
             </div>
