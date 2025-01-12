eba3782454216ba2f6cee95d22fe3fb1aec993f1
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-12 13:18:46 +0000

    Improved: Typography

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="Header flex items-center justify-between size-a" id="Header">
             <div className='flex pointer-events-none z-1 items-center select-none'>
                 <img className='King pointer-events-none w-16 lg:w-23 inline-block' src='/King.png' alt="King Image" />
-                <h3 className="Name Roboto-Condensed font-size-4 lg:font-size-6 inline-block vertical-middle text-white cursor-default">Λʙʜιנєєτ  Ðʜικαℓє</h3>
+                <pre className="Name Roboto-Condensed font-size-4 lg:font-size-6 inline-block vertical-middle text-white cursor-default">Λ ʙ ʜ ι נ є є τ   Ð ʜ ι κ α ℓ є</pre>
             </div>
             <a href="/Documents/AbhijeetPrakashDhikaleResume.pdf" target='_blank' className="Resume z-1 font-size-4">Resume</a>
         </div>
