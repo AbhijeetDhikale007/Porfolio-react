751c096f128022568373731219f61c9759e1ff02
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-13 11:41:52 +0000

    Added: Projects Icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = () => {
     <>
       <div className="Center pointer-events-none">
         <nav className="Navbar pointer-events-auto fixed flex">
-          <Link className="tooltip" data-title="Home" to="Header" spy={true} smooth={true} offset={-20} duration={100}><img className='w-8' src="/assets/PNG/Home.png" alt="Home" /></Link>
-          <Link className="tooltip" data-title="About" to="About" spy={true} smooth={true} duration={100}><img className='w-8' src="/assets/PNG/About.png" alt="About" /></Link>
-          <Link className="tooltip" data-title="Education" to="Education" spy={true} smooth={true} duration={100}><img className='w-8' src="/assets/PNG/Education.png" alt="Education" /></Link>
-          <Link className="tooltip" data-title="Skills" to="Skills" spy={true} smooth={true} duration={100}><img className='w-8' src="/assets/PNG/Skills.png" alt="Skills" /></Link>
-          <Link className="tooltip" data-title="Projects" to="Projects" spy={true} smooth={true} duration={100}><img className='w-8' src="/assets/PNG/Projects.png" alt="Projects" /></Link>
-          <Link className="tooltip" data-title="Contact" to="Contact" spy={true} smooth={true} duration={100}><img className='w-8' src="/assets/PNG/Contact.png" alt="Contact" /></Link>
+          <Link className="tooltip" data-title="Home" to="Header" spy={true} smooth={true} offset={-20} duration={100}><img className='w-8' src='/assets/PNG/Home.png' alt='Home' /></Link>
+          <Link className="tooltip" data-title="About" to="About" spy={true} smooth={true} duration={100}><img className='w-8' src='/assets/PNG/About.png' alt='About' /></Link>
+          <Link className="tooltip" data-title="Education" to="Education" spy={true} smooth={true} duration={100}><img className='w-8' src='/assets/PNG/Education.png' alt='Education' /></Link>
+          <Link className="tooltip" data-title="Skills" to="Skills" spy={true} smooth={true} duration={100}><img className='w-8' src='/assets/PNG/Skills.png' alt='Skills' /></Link>
+          <Link className="tooltip" data-title="Projects" to="Projects" spy={true} smooth={true} duration={100}><img className='w-8' src='/assets/PNG/Projects.png' alt='Projects' /></Link>
+          <Link className="tooltip" data-title="Contact" to="Contact" spy={true} smooth={true} duration={100}><img className='w-8' src='/assets/PNG/Contact.png' alt='Contact' /></Link>
         </nav>
       </div>
     </>
