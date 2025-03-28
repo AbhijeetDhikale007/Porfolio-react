1727b09943ccc1cb159ff715e28c4d5a78363985
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-28 08:20:06 +0000

    Old Resume Deleted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <img className='King pointer-events-none w-16 lg:w-23 inline-block' src='/King.png' alt="King Image" />
                 <pre className="Name Roboto-Condensed font-size-4 lg:font-size-6 inline-block vertical-middle text-white cursor-default">Λ ʙ ʜ ι נ є є τ   Ð ʜ ι κ α ℓ є</pre>
             </div>
-            <a href="/Documents/AbhijeetPrakashDhikaleResume.pdf" target='_blank' className="Resume z-1 font-size-4">Resume</a>
+            <a href="/Documents/Abhijeet-Prakash-Dhikale-Resume.pdf" target='_blank' className="Resume z-1 font-size-4">Resume</a>
         </div>
     )
 }
