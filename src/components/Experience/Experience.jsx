fcf4b78e1527df8370f96e6947d557e0bda11f42
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-29 08:56:18 +0000

    Details Change

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,7 +2,7 @@ import './Experience.scss'
 
 const Experiences = [
     { CertificateUrl: 'Documents/Internship - Calanjiyam Consultancy.pdf', imgUrl: 'assets/Education/Calanjiyam.png', imgAlt: 'Calanjiyam Consultancies and Technologies', Title: 'Full stack Web application Development Intern', Company: 'Calanjiyam Consultancies and Technologies', Location: 'Erode, Tamil Nadu - 638455', Date: '3 Jan 2025 - 5 Mar 2025', Duration: '8 Weeks', Details: 'An internship on Full stack Web application Development using HTML, CSS, JavaScript, jQuery, Ajax, PHP, and MySQL at Calanjiyam Consultancies and Technologies (8 Weeks). The internship was centred around the following project ‘E - learning platform’.' },
-    { CardClass: 'CardEdu Card2', CertificateUrl: 'Documents/ITI-ConsolidatedMarksheet_R180827051938.pdf', imgUrl: 'assets/Images/MVP.png', imgAlt: 'MVP', Title: 'Industrial Training Institute', Company: `NDMVP Samaj's ITI`, Location: 'Nashik, Maharashtra', Date: 'Aug 2018 - May 2021', Duration: '2 Year', Details: '' },
+    // { CertificateUrl: 'Documents/ITI-ConsolidatedMarksheet_R180827051938.pdf', imgUrl: 'assets/Images/MVP.png', imgAlt: 'MVP', Title: 'Industrial Training Institute', Company: `NDMVP Samaj's ITI`, Location: 'Nashik, Maharashtra', Date: 'Aug 2018 - May 2021', Duration: '2 Year', Details: '' },
 ]
 
 const Experience = () => {
